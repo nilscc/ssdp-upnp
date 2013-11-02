92f94ad8748e4fa905264615af026db98c0386c2
Nils Schweinsberg <mail@nils.cc>
2013-11-02 21:06:30 +0100

    updated examples/"tests"

--- src/Test.hs
+++ src/Test.hs
@@ -1,19 +1,45 @@
 module Main where
 
+import Text.XML.Light
+
 import Network
 import Network.SSDP
+import Network.UPnP
+import Network.UPnP.Types
 
 discover :: IO ()
 discover = do
-  results <- sendSearch ssdp callback
+  results <- sendSearch ssdp
   mapM_ put results
  where
   ssdp = ssdpSearch UpnpRootDevice Nothing Nothing
-  callback from msg = return (from, msg)
-  put (from, msg) =
-    putStrLn $ "\nMsg [ " ++ show from ++ " ]:\n\n"
-               ++ render msg
+  put (from, msg) = do
+    putStrLn $ "\nMsg [ " ++ show from ++ " ]:\n"
+    putStrLn $ "Server:   " ++ show (getHeaderValue "SERVER" msg)
+    putStrLn $ "Location: " ++ show (getHeaderValue "LOCATION" msg)
+    putStrLn $ "\n" ++ render msg
+
+getDescOfFirst :: IO ()
+getDescOfFirst = do
+
+  -- SSDP search
+  res <- sendSearch ssdp
+  let ((from, notify):_) = filter (hasHeader "LOCATION" . snd) res
+
+  putStrLn $ "\nMsg [ " ++ show from ++ " ]:\n"
+  putStrLn $ "Server:   " ++ show (getHeaderValue "SERVER" notify)
+  putStrLn $ "Location: " ++ show (getHeaderValue "LOCATION" notify)
+  putStrLn $ "\n" ++ render notify
+
+  -- get description
+  Just (UpnpXml cont) <- requestDeviceDescription notify
+
+  putStrLn "\nDevice description:\n"
+  mapM_ (putStrLn . ppContent) cont
+ where
+  ssdp = ssdpSearch UpnpRootDevice Nothing Nothing
 
 main :: IO ()
 main = withSocketsDo $ do
-  discover
+  --discover
+  getDescOfFirst
