dd947ecda3b160bca18f736c163b60a09b2441dd
Nils Schweinsberg <mail@nils.cc>
2013-11-03 00:33:15 +0100

    cosmetics in Test.hs

--- src/Test.hs
+++ src/Test.hs
@@ -28,11 +28,10 @@ showDeviceInfo dev = do
 
 discover :: IO ()
 discover = do
+  let ssdp = ssdpSearch UpnpRootDevice Nothing Nothing
   results <- sendSearch ssdp
-  mapM_ put results
- where
-  ssdp = ssdpSearch UpnpRootDevice Nothing Nothing
-  put (from, msg) = do
+  forM_ results $ \(from, msg) -> do
+
     putStrLn $ "\nMsg [ " ++ show from ++ " ]:\n"
     putStrLn $ "Server:   " ++ show (getHeaderValue "SERVER" msg)
     putStrLn $ "Location: " ++ show (getHeaderValue "LOCATION" msg)
@@ -42,8 +41,9 @@ getDescOfFirstIGD :: IO ()
 getDescOfFirstIGD = do
 
   -- SSDP search
+  let ssdp = ssdpSearch (UrnDevice "schemas-upnp-org" "InternetGatewayDevice" "1")
+                        Nothing Nothing
   res <- sendSearch ssdp
-  print $ length res
   let ((from, notify):_) = filter (hasHeader "LOCATION" . snd) res
 
   putStrLn $ "\nMsg [ " ++ show from ++ " ]:\n"
@@ -53,15 +53,13 @@ getDescOfFirstIGD = do
   Just dev <- requestDeviceDescription notify
   showDeviceInfo dev
 
- where
-  ssdp = ssdpSearch (UrnDevice "schemas-upnp-org" "InternetGatewayDevice" "1") Nothing Nothing
-
 findWANIPConnection1s :: IO ()
 findWANIPConnection1s = do
   let ssdp = ssdpSearch UpnpRootDevice Nothing Nothing
   results <- sendSearch ssdp
 
   forM_ results $ \(_from, notify) -> do
+
     dev <- requestDeviceDescription notify
     let wanip1 = StandardService "WANIPConnection" "1"
     case join $ findService wanip1 <$> dev of
